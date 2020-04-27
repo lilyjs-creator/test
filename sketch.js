2426e7337c58250020d4a1031bc5f56da32de947
lilyjs-creator <ananyadalmia2003@gmail.com>
2020-04-27 20:00:20 +0530

    change made constaraint created

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,constrainedlog,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,7 +17,7 @@ function setup(){
     world = engine.world;
 
 
-    ground = new Ground(150,height,1200,20);
+    ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
@@ -35,7 +36,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    
+    constrainedlog = new Log(230,180,80,PI/2);
 
+    chain = new Chain(bird.body,constrainedlog.body);
 }
 
 function draw(){
@@ -61,4 +65,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    
+    constrainedlog.display();
+    chain.display();
 }
